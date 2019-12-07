60512b50cabbcc53b36aadefb4c52cb08867f035
Lillian Bitnr <lillian.bitner@gmail.com>
2019-12-07 04:57:40 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,38 @@
+:sentence1.each do |sentence1|
+  puts :sentence1 
+  reverse_each_word = :sentence1.reverse do |sentence1| 
+  puts sentence1
+:sentence2.each do |sentence2|
+  puts :sentence2
+  reverse_each_word = :sentence2.reverse do |sentence2| 
+  puts sentence2 
+:sentence1.collect do |sentence1| 
+  puts :sentence1 
+:sentence2.collect do |sentence2|
+  puts :sentence2 
+end 
+end 
+end 
+end 
+
+
+
+
+
+
+describe '#reverse_each_word' do
+  let(:sentence1) { "Hello there, and how are you?" }
+  it 'reverses all the words in a string without reversing the order of the words' do
+    expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
+  end
+
+  let(:sentence2) { "Hi again, just making sure it's reversed!" }
+  it 'reverses all the words in another string without reversing the order of the words' do
+    expect(reverse_each_word(sentence2)).to eq("iH ,niaga tsuj gnikam erus s'ti !desrever")
+  end
+
+  it 'uses collect' do
+    expect_any_instance_of(Array).to receive(:collect).and_call_original
+    reverse_each_word("Verifying that collect is being called.")
+  end
+end
